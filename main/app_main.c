18b7f105ffe27625ecdf2afa2b5a5b82a9a65164
Balazs Bence <balazs.bence@student.ms.sapientia.ro>
2024-10-29 10:39:56 +0200

    Main Update

--- main/app_main.c
+++ main/app_main.c
@@ -1,11 +1,13 @@
+// app_main.c
 #include "nvs_flash.h"
 #include "mqtt.h"
 #include "szenzor.h"
+#include "gps.h"
 #include "esp_log.h"
 
 void app_main(void)
 {
-    // Initialize NVS
+    // NVS inicializálása
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK(nvs_flash_erase());
@@ -14,7 +16,14 @@ void app_main(void)
     ESP_ERROR_CHECK(ret);
 
     ESP_LOGI("Main", "ESP_WIFI_MODE_STA");
-    wifi_init_sta();  // Initialize WiFi connection
+    wifi_init_sta();  // WiFi kapcsolat inicializálása
 
-    mqtt_app_start();  // Initialize and start MQTT
+    mqtt_app_start();  // MQTT inicializálása és indítása
+    
+    gps_init();  // GPS inicializálása
+
+    while (1) {
+        gps_read_and_publish();  // GPS adat beolvasása és publikálása
+        vTaskDelay(pdMS_TO_TICKS(1000));  // 1 másodperces késleltetés
+    }
 }
